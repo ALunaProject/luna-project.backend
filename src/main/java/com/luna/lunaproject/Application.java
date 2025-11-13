e4d793c88437447abaffb1c85301ceaef0da1dc6
lyanbrito <lyanbrito613@gmail.com>
2025-11-13 10:20:47 -0300

    feat: initialized Spring project

---
+++ src/main/java/com/luna/lunaproject/Application.java
@@ -0,0 +1,13 @@
+package com.luna.lunaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
