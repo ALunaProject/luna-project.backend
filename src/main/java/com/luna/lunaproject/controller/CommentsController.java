27a0f6c652d9fb2ac445a2927639952e661a53f3
Lyan Brito <lyanbrito613@gmail.com>
2025-12-05 15:16:11 -0300

    fix: updated comments controller

--- src/main/java/com/luna/lunaproject/controller/CommentsController.java
+++ src/main/java/com/luna/lunaproject/controller/CommentsController.java
@@ -1,46 +1,49 @@
 package com.luna.lunaproject.controller;
 
-
 import com.luna.lunaproject.dto.CommentsReqDTO;
 import com.luna.lunaproject.dto.CommentsResDTO;
+import com.luna.lunaproject.entity.Comment;
+import com.luna.lunaproject.repository.CommentRepo;
 import com.luna.lunaproject.services.CommentsServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/comments")
+@RequestMapping("/api/comments")
 public class CommentsController {
 
-    public final CommentsServices commentsServices;
+    private final CommentsServices services;
 
-    public CommentsController(CommentsServices commentsServices) {
-        this.commentsServices = commentsServices;
+    public PostController(CommentsServices services) {
+        this.services = services;
     }
 
-    @GetMapping(value = "getAll")
-    public ResponseEntity<?> getAllComments() {
-        return ResponseEntity.ok(commentsServices.getAllComments());
+    @GetMapping("/display")
+    public List<CommentsResDTO> getAll() {
+        return services.getAllComments();
     }
 
-    @PostMapping(value = "newComment")
-    public ResponseEntity<?> addComment(CommentsReqDTO req) {
-        return ResponseEntity.ok(commentsServices.saveComment(req));
+    @GetMapping("/display/{id}")
+    public List<CommentsResDTO> getCommentId(@PathVariable int id) {
+        return services.getCommentById(id);
     }
 
-    @GetMapping(value = "getById")
-    public ResponseEntity<List<CommentsResDTO>> getCommentById(int id, CommentsReqDTO req) {
-        return ResponseEntity.ok(commentsServices.getCommentById(id));
+    @PostMapping("/newComment")
+    public ResponseEntity<?> newComment(@RequestBody CommentsReqDTO req) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(services.saveComment(req));
     }
 
-    @PutMapping(value = "edit")
-    public ResponseEntity<?> editComment(@RequestBody CommentsReqDTO req, int id) {
-        return ResponseEntity.ok(commentsServices.editComment(id, req));
+    @PutMapping("/edit/{id}")
+    public ResponseEntity<?> editComment(@RequestBody CommentsReqDTO req, @PathVariable int id) {
+        return ResponseEntity.ok(services.editComment(id, req));
     }
 
-    @DeleteMapping("delete")
-    public ResponseEntity<String> delete(String res) {
-        return ResponseEntity.ok(res);
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteComments(@PathVariable int id) {
+        return ResponseEntity.ok(services.deletePost(id));
     }
 }
