ae4c97b8fe794684fb92dfd886dc5457b554c8cc
Lucas Wendell <lucas.wendell.98@gmail.com>
2026-08-25 23:21:54 -0300

    feat: created user entity and dtos

--- src/main/java/com/luna/lunaproject/domain/entity/User.java
+++ src/main/java/com/luna/lunaproject/domain/entity/User.java
@@ -1,14 +1,23 @@
 package com.luna.lunaproject.domain.entity;
 
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.UUID;
 
 @Data
+@Entity(name = "user")
+@NoArgsConstructor
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+    @Column(unique = true)
     private String username;
     private String email;
     private String password;
