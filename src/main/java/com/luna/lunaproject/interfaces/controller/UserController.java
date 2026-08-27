e5a371ceb4cc184fab4ea89654bb946f01565181
lucaswgoncalves616 <lucas.wendell.98@gmail.com>
2026-08-27 08:47:07 -0300

    refactor: removed unused imports

--- src/main/java/com/luna/lunaproject/interfaces/controller/UserController.java
+++ src/main/java/com/luna/lunaproject/interfaces/controller/UserController.java
@@ -5,7 +5,6 @@ import com.luna.lunaproject.application.dto.UserResponseDto;
 import com.luna.lunaproject.application.dto.UserUpdateDto;
 import com.luna.lunaproject.application.services.UserService;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
