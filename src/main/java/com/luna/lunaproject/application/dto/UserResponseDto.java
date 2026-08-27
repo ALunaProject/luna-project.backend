e774f4976e92ea09d998cf35033d9fb5db28d9fd
Lucas Wendell <lucas.wendell.98@gmail.com>
2026-08-26 21:32:55 -0300

    feat: created user dtos

---
+++ src/main/java/com/luna/lunaproject/application/dto/UserResponseDto.java
@@ -0,0 +1,26 @@
+package com.luna.lunaproject.application.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class UserResponseDto {
+
+    private UUID id;
+    private String username;
+    private String email;
+    private String profilePicUrl;
+    private String bannerUrl;
+
+
+    public UserResponseDto(UUID id, String username, String email) {
+    }
+
+    public UserResponseDto(UUID id, String username) {
+    }
+}
--- src/main/java/com/luna/lunaproject/application/dto/UserUpdateDto.java
+++ src/main/java/com/luna/lunaproject/application/dto/UserUpdateDto.java
@@ -1,25 +1,22 @@
 package com.luna.lunaproject.application.dto;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.validator.constraints.Length;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class CreateUserDto {
+public class UserUpdateDto {
 
     @NotBlank
     @Pattern(regexp = "\\S+", message = "O campo [username] não pode ter espaço")
     private String username;
-    @Email(message = "O campo [email] deve conter um e-mail válido")
-    private String email;
-    @Length(min = 6, max = 100, message = "A senha deve ter entre 6 a 100 caracteres.")
-    private String password;
+    private String profilePicUrl;
+    private String bannerUrl;
+
 }
