57b3b24d505fc9f98dcf24994a5cc23d7a4981fb
nathaliyax <nathalialimas@hotmail.com>
2026-08-30 15:04:52 -0300

    feat: implementing role

--- src/main/java/com/luna/lunaproject/domain/entity/User.java
+++ src/main/java/com/luna/lunaproject/domain/entity/User.java
@@ -1,16 +1,22 @@
 package com.luna.lunaproject.domain.entity;
 
 
+import com.luna.lunaproject.domain.enums.UserRole;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Data
 @Entity(name = "user")
 @NoArgsConstructor
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -21,4 +27,15 @@ public class User {
     private String password;
     private String profilePicUrl;
     private String bannerUrl;
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+        if (this.role == UserRole.ADMIN) {
+            return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        }else {
+            return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+        }
+    }
 }
