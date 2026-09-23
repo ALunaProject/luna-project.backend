e6b44f8a612bb31995e5e4b91252fa6724f508b1
Bh0Eri <ericmoreiradosreis@gmail.com>
2026-09-23 00:29:17 -0300

    refactor(comment): update CommentRepository key type to UUID

--- src/main/java/com/luna/lunaproject/domain/repository/CommentRepository.java
+++ src/main/java/com/luna/lunaproject/domain/repository/CommentRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.UUID;
 
-public interface CommentRepository extends JpaRepository<Comment, Long> {
+public interface CommentRepository extends JpaRepository<Comment, UUID> {
 
     // Lista os comentários de um post, do mais antigo para o mais novo
     List<Comment> findByPostIdOrderByCreationDateAsc(UUID postId);
